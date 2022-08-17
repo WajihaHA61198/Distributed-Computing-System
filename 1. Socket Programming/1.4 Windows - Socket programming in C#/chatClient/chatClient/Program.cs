03874d3c967be36f8d3026c91355b9be19b131d1
Wajiha Arain <88322471+WajihaHA61198@users.noreply.github.com>
2022-08-17 14:48:03 +0500

    Add files via upload

---
+++ 1. Socket Programming/1.4 Windows - Socket programming in C#/chatClient/chatClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace chatClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            s.Connect(IPAddress.Parse("10.50.2.222"),4444);
+
+            Random rn = new Random();
+            string msg = "Hello I am chatClient " + rn.Next(100).ToString();
+            byte[] msg_sent = Encoding.ASCII.GetBytes(msg);
+            s.Send(msg_sent, 0);
+
+            //now receive in the same buffer
+            byte[] msg_recv = new byte[250];
+            int cr = s.Receive(msg_recv);
+            msg = Encoding.ASCII.GetString(msg_recv, 0, cr);
+            Console.Write(msg);
+        }
+    }
+}
---
+++ 1. Socket Programming/1.4 Windows - Socket programming in C#/chatServer/chatServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace chatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am Chat Server and listening to 5 clients");
+            Socket s = new Socket(AddressFamily.InterNetwork, 
+                                   SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint server = new IPEndPoint(IPAddress.Parse("10.50.2.222"), 4444);
+            s.Bind(server);
+            s.Listen(5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Socket c = s.Accept();
+                //Receive Message
+                byte[] msg_recv = new byte[250];
+                int cr = c.Receive(msg_recv);
+                string msg = Encoding.ASCII.GetString(msg_recv, 0, cr);
+                Console.WriteLine("Message Received from : " + 
+                               c.RemoteEndPoint.ToString() + " = " + msg);
+                //Send Message
+                msg = "Hello I am chatServer : and you are " + c.RemoteEndPoint.ToString();
+                byte[] msg_sent = Encoding.ASCII.GetBytes(msg);
+                c.Send(msg_sent, 0);
+                
+                c.Close();
+            }
+        }
+    }
+}
